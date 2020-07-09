2bf279ddbbaf19e87e864a3a539cdec611e73154
Rodrigo Barbosa <rodrigo.barba.88@gmail.com>
2020-07-08 21:00:09 -0300

    removendo node_modules

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -13,20 +13,6 @@ export class UsuarioFormComponent {
 
   onSubmit() { this.submitted = true; }
 
-  // powers = ['Really Smart', 'Super Flexible',
-  //           'Super Hot', 'Weather Changer'];
-  // model = new Usuario(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
-  // TODO: Remove this when we're done
-  // get diagnostic() { return JSON.stringify(this.model); }
-  // newUsuario() {
-  //   this.model = new Usuario(42, '', '');
-  // }
-  // skyDog(): Usuario {
-  //   let myUsuario =  new Usuario();
-  //   console.log('My Usuario is called ' + myUsuario.username); // "My Usuario is called SkyDog"
-  //   return myUsuario;
-  // }
-
   //////// NOT SHOWN IN DOCS ////////
 
   // Reveal in html:
